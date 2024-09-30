a0ea65f4529295352d8ebc977ee86902b6623cfc
rahulrai0001 <rr326990@gmail.com>
2024-09-30 11:39:08 +0530

    add line above text

--- script/todo.js
+++ script/todo.js
@@ -1,58 +1,68 @@
+myArray = JSON.parse(localStorage.getItem('todoList')) || [{ name: 'add some work', duedate: '2024-09-04' }];
 
-        myArray = JSON.parse(localStorage.getItem('todoList')) || [ {name:'add some work', duedate:'2024-09-04'}];
+rendertodo();
 
-        
-        rendertodo();
-        function rendertodo(){
-            display = '';
-            for (let i = 0; i < myArray.length; i++) {
-                const todoobject = myArray[i];
-                const {name, duedate} = todoobject;
+function rendertodo() {
+    let display = '';
+    for (let i = 0; i < myArray.length; i++) {
+        const todoobject = myArray[i];
+        const { name, duedate } = todoobject;
 
-                const element = `
-                <div class="todo-item">
-                    <span class="task">${name}</span>
-                    <span class="date">${duedate}</span>
-                    <button class="delete-btn" onclick="
-                        myArray.splice(${i}, 1); 
-                        rendertodo();
-                         saveToStorage();
-                    ">DELETE</button>
-                </div>`;
-                
-                display += element;
-            }
+        // Check if the task is marked as done in localStorage
+        const isDone = todoobject.done ? 'checked' : '';
 
-            document.querySelector('.to-do-show').innerHTML = display;
+        const element = `
+        <div class="todo-item">
+            <input type='checkbox' ${isDone} onchange="workDone(${i});"> 
+            <span class="task ${isDone ? 'completed' : ''}">${name}</span>
+            <span class="date">${duedate}</span>
+            <button class="delete-btn" onclick="
+                myArray.splice(${i}, 1); 
+                rendertodo();
+                saveToStorage();
+            ">DELETE</button>
+        </div>`;
+
+        display += element;
+    }
+
+    document.querySelector('.to-do-show').innerHTML = display;
+}
+
+function workDone(index) {
+    // Toggle the 'done' property in the array for the specific task
+    myArray[index].done = !myArray[index].done;
+    saveToStorage();
+    rendertodo();
+}
+
+function handlekeydown(event) {
+    if (event.key === 'Enter') {
+        todoWork();
+    }
+}
+
+function todoWork() {
+    let inputElement = document.querySelector('input').value;
+    let inputDateElement = document.querySelector('#date').value;
+
+    if (inputElement === '' || inputDateElement === '') {
+        alert("Please fill in both the text and date fields.");
+    } else {
+        let index = myArray.findIndex(item => item.name === 'add some work');
+        if (index !== -1) {
+            myArray.splice(index, 1);
         }
 
-        function handlekeydown(event){
-            if (event.key === 'Enter'){
-                todoWork();
-            }
-        }
+        myArray.push({ name: inputElement, duedate: inputDateElement, done: false });
+    }
 
-        function todoWork(){
-            let inputElement = document.querySelector('input').value;
-            let inputDateElement = document.querySelector('#date').value;
+    saveToStorage();
+    document.querySelector('input').value = '';
+    document.querySelector('#date').value = '';
+    rendertodo();
+}
 
-            if (inputElement === '' || inputDateElement === '') {
-                alert("Please fill in both the text and date fields.");
-            } else {
-                let index = myArray.findIndex(item => item.name === 'add some work');
-                if (index !== -1) {
-                    myArray.splice(index, 1);
-                }
-
-                myArray.push({name: inputElement, duedate: inputDateElement});
-            }
-
-            saveToStorage();
-            document.querySelector('input').value = '';
-            document.querySelector('#date').value = '';
-            rendertodo();
-        }
-    
-        function saveToStorage() {
-            localStorage.setItem('todoList', JSON.stringify(myArray));
-          }
+function saveToStorage() {
+    localStorage.setItem('todoList', JSON.stringify(myArray));
+}
